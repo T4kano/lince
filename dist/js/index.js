669be1af497b3a7ff1bc35acc2e0206651cf59be
T4kano <edutakajr@gmail.com>
2021-10-27 11:08:01 -0300

    commit

---
+++ dist/js/index.js
@@ -0,0 +1,45 @@
+class MobileNavbar {
+  constructor(toggler, navList, navLinks) {
+    this.toggler = document.querySelector(toggler);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation ?
+        (link.style.animation = "") :
+        (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                  index / 7 + 0.3
+              }s`);
+    })
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.toggler.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.toggler.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.toggler) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".nav__toggler",
+  ".nav__list",
+  ".nav__list li",
+)
+
+mobileNavbar.init();
